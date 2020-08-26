6b74b39c9776d13fab02923d05978c8a5ac039c3
Yuri Anderson <yuri.anderson.lobo@gmail.com>
2020-08-26 14:42:27 -0300

    Implementando bd  e indices dinamicos

--- app.js
+++ app.js
@@ -9,6 +9,27 @@ class Despesa{
 
 	}
 }
+class Bd{
+
+	constructor(){
+		let id = localStorage.getItem('id')
+
+		if(id === null){
+			localStorage.setItem('id',0)
+		} 
+	}
+	getProximoId(){
+		let proximoId =localStorage.getItem('id')
+		return parseInt(proximoId)+1
+	}
+	gravar(d){
+		//localStorage.setItem('despesa',JSON.stringify(d))
+		let id = this.getProximoId()
+		localStorage.setItem(id, JSON.stringify(d))
+		localStorage.setItem('id',id)
+	}
+}
+let bd =new Bd()
 
 function cadastrarDespesa(){
 
@@ -23,9 +44,10 @@ function cadastrarDespesa(){
 	
 	let despesa = new Despesa(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)
 
-	gravar(despesa)
+	bd.gravar(despesa)
 
 }
-function gravar(d){
-	localStorage.setItem('despesa',JSON.stringify(d))
-}
+
+
+
+
